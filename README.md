4fb507f5ab049042d31132e027612da79dd6edfc
victor <fedorenko22116@gmail.com>
2019-02-03 15:21:30 +0200

    corrected params customization

---
+++ README.md
@@ -0,0 +1,15 @@
+# Webchain daemon - docker image
+
+Simple docker wrapper for webchain daemon to use in your project.
+
+## Variables
+
+| var | default | description |
+| --- | ------- | ----------: |
+| PASS | /data/password | Text file with an acconts password |
+| ACCOUNTS | /data/private | Account private key |
+| IS_FAST | 1 | Use fast node |
+| RPC_HOST | "0.0.0.0" | Host for RPC API |
+| RPC_PORT | 39573 | Port for RPC API |
+| RPC_CORS | "*" | CORS for RPC API |
+| RPC_API | "admin,debug,webchain,miner,eth,net,web3" | Permissions to use in API |
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
-webchaind --password /data/password account import /data/private
-webchaind --fast \
+_PASS=${PASS:=/data/password}
+_ACCOUNTS=${ACCOUNTS:=/data/private}
+_IS_FAST=${IS_FAST:=1}
+_RPC_HOST=${RPC_HOST:="0.0.0.0"}
+_RPC_PORT=${RPC_PORT:=39573}
+_RPC_CORS=${RPC_CORS:="*"}
+_RPC_API=${RPC_API:="admin,debug,webchain,miner,eth,net,web3"}
+
+webchaind --password ${_PASS} account import ${_ACCOUNTS}
+webchaind if [[${_IS_FAST} -eq 1]] then --fast fi \
     --rpc \
-    --rpc-addr "0.0.0.0" \
-    --rpc-port 39573 \
-    --rpc-cors-domain "*" \
-    --rpc-api="admin,debug,webchain,miner,eth,net,web3" \
+    --rpc-addr ${_RPC_HOST} \
+    --rpc-port ${_RPC_PORT} \
+    --rpc-cors-domain ${_RPC_CORS} \
+    --rpc-api=${_RPC_API} \
     --unlock 0 \
-    --password /data/password
+    --password ${_PASS}
